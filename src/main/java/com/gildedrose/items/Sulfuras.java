c62211c42b87f78ebc5ba6c76b2d016e4a007b56
mertakl <akelmert95@gmail.com>
2021-11-29 15:24:08 +0300

    Implemented Sulfuras.java

--- src/main/java/com/gildedrose/items/Sulfuras.java
+++ src/main/java/com/gildedrose/items/Sulfuras.java
@@ -1,4 +1,20 @@
 package com.gildedrose.items;
 
-public class Sulfuras extends NormalItem {
+import com.gildedrose.Item;
+import com.gildedrose.ItemUpdater;
+
+public class Sulfuras implements ItemUpdater {
+
+    @Override
+    public void updateItemQuality(Item item) {
+        //"Sulfuras" is a legendary item and as such its Quality is 80 and it never alters.
+        if (item.quality != 80) {
+            item.quality = 80;
+        }
+    }
+
+    @Override
+    public void decreaseSellIn(Item item) {
+        //"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+    }
 }
